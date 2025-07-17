627460b644527c09e530309a5d83cfdfb748fef7
shaikmohammadsameer22 <sameerabbas61434@gmail.com>
2025-07-18 01:02:56 +0530

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,6 @@
-// vite.config.js
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Load .env file
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env, // makes VITE_ variables available
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
 });
